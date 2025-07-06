42f9324f0df31037891d176ad6769bdce7242d4f
Kiran Kumar <kirankiru1936@gmail.com>
2025-07-07 00:30:19 +0530

    Updated App.js with new feature

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Happy Coding !
+          Learn React and Happy Coding ! By learning clear Js Concepts#
         </a>
       </header>
     </div>
